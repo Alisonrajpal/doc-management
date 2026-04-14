0bd20be7179ed1f262e698638cffd52795c3b4ae
Alison Rajpal <135074591+Alisonrajpal@users.noreply.github.com>
2026-04-14 13:46:39 +0200

    update the react statement

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getDocuments } from "../lib/supabase";
 import { supabase } from "../lib/supabase";
 import {
